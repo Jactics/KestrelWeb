fa130ecf49544d66aba7e0c5ad5ad2112d5af006
Tyrus <jactiction@gmail.com>
2020-04-19 23:40:13 +0800

    v1

---
+++ Program.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using System.Net;
+
+namespace KestrelWeb
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((context, services) =>
+                {
+                    services.Configure<KestrelServerOptions>(
+                        context.Configuration.GetSection("Kestrel"));
+                })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.ConfigureKestrel(serverOptions =>
+                    {
+                        serverOptions.Limits.MaxConcurrentConnections = 100;
+                        serverOptions.Limits.MaxConcurrentUpgradedConnections = 100;
+                        serverOptions.Limits.MaxRequestBodySize = 10 * 1024;
+                        serverOptions.Limits.MinRequestBodyDataRate =
+                            new MinDataRate(bytesPerSecond: 100,
+                                gracePeriod: TimeSpan.FromSeconds(10));
+                        serverOptions.Limits.MinResponseDataRate =
+                            new MinDataRate(bytesPerSecond: 100,
+                                gracePeriod: TimeSpan.FromSeconds(10));
+                        serverOptions.Listen(IPAddress.Loopback, 5000);
+                        //serverOptions.Listen(IPAddress.Loopback, 5001,
+                        //    listenOptions =>
+                        //    {
+                        //        listenOptions.UseHttps("testCert.pfx",
+                        //            "testPassword");
+                        //    });
+                        serverOptions.Limits.KeepAliveTimeout =
+                            TimeSpan.FromMinutes(2);
+                        serverOptions.Limits.RequestHeadersTimeout =
+                            TimeSpan.FromMinutes(1);
+                        serverOptions.ConfigureEndpointDefaults(listenOptions =>
+                        {
+
+                        });
+                        //serverOptions.ConfigureHttpsDefaults(ListenOptions =>
+                        //{
+                        //    ListenOptions.ServerCertificate = "X509Certificate2";
+                        //});
+                    });
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
